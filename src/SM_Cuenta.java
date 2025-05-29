29db5dba4900b0de462133e7d36e63a338500efe
sofi161mar <sofialem2006@gmail.com>
2025-05-29 16:32:34 -0500

    Commit 3

--- src/SM_Cuenta.java
+++ src/SM_Cuenta.java
@@ -1,2 +1,25 @@
 public class SM_Cuenta {
+    protected float saldo;
+    protected int numDepositos;
+    protected int numRetiros;
+    protected float tasaAnual;
+
+    public SM_Cuenta(float saldoInicial, float tasaAnual) {
+        this.saldo = saldoInicial;
+        this.tasaAnual = tasaAnual;
+        this.numDepositos = 0;
+        this.numRetiros = 0;
+    }
+
+    public void depositar(float monto) {
+        if (monto > 0) {
+            saldo += monto;
+            numDepositos++;
+            System.out.println("Depósito exitoso.");
+        } else {
+            System.out.println("El monto debe ser positivo.");
+        }
+    }
+
+
 }
--- src/SM_CuentaAhorros.java
+++ src/SM_CuentaAhorros.java
@@ -1,2 +1,6 @@
 public class SM_CuentaAhorros {
+    public SM_CuentaAhorros(float saldoInicial, float tasaAnual) {
+        super(saldoInicial, tasaAnual);
+    }
+
 }
